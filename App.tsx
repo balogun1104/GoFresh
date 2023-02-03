86f35ce5fedb3d01cabf2c5e6b46b538b6fc5921
Balogun Razak <razak2free@gmail.com>
2023-02-03 21:56:10 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,16 +2,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LandingScreen from "./src/screens/LandingScreen";
-import { createAppContainer,  createSwitchNavigator } from "react-navigation";
 
 
-import { createStackNavigator } from 'react-navigation-stack'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { SearchScreen } from './src/screens/SearchScreen';
-import { RestaurantScreen } from './src/screens/RestaurantScreen';
-import { FoodDetailScreen } from './src/screens/FoodDetailScreen';
-import { CartScreen } from './src/screens/CartScreen';
-import { LoginScreen } from './src/screens/LoginScreen';
+
 
 
 
